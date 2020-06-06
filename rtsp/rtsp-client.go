bdd8eb85c3f2b1580fd5d8843917f5eb3dbc3ba1
snwolyg <569616226@qq.com>
2020-06-06 17:32:47 +0800

    增加 Basic 认证方式

--- rtsp/rtsp-client.go
+++ rtsp/rtsp-client.go
@@ -136,6 +136,37 @@ func DigestAuth(authLine string, method string, URL string) (string, error) {
 	return Authorization, nil
 }
 
+func BasicAuth(authLine string, method string, URL string) (string, error) {
+	l, err := url.Parse(URL)
+	if err != nil {
+		return "", fmt.Errorf("Url parse error:%v,%v", URL, err)
+	}
+	realm := ""
+	nonce := ""
+	realmRex := regexp.MustCompile(`realm="(.*?)"`)
+	result1 := realmRex.FindStringSubmatch(authLine)
+
+	if len(result1) == 2 {
+		realm = result1[1]
+	} else {
+		return "", fmt.Errorf("auth error : no realm found")
+	}
+
+	// response= md5(md5(username:realm:password):nonce:md5(public_method:url));
+	username := l.User.Username()
+	password, _ := l.User.Password()
+	l.User = nil
+	if l.Port() == "" {
+		l.Host = fmt.Sprintf("%s:%s", l.Host, "554")
+	}
+	md5UserRealmPwd := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s:%s", username, realm, password))))
+	md5MethodURL := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s", method, l.String()))))
+
+	response := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s:%s", md5UserRealmPwd, nonce, md5MethodURL))))
+	Authorization := fmt.Sprintf("Basic username=\"%s\", realm=\"%s\", uri=\"%s\", response=\"%s\"", username, realm, l.String(), response)
+	return Authorization, nil
+}
+
 func (client *RTSPClient) checkAuth(method string, resp *Response) (string, error) {
 	if resp.StatusCode == 401 {
 
@@ -147,13 +178,13 @@ func (client *RTSPClient) checkAuth(method string, resp *Response) (string, erro
 			for _, authLine := range auths {
 
 				if strings.IndexAny(authLine, "Digest") == 0 {
-					// 					realm="HipcamRealServer",
+					// realm="HipcamRealServer",
 					// nonce="3b27a446bfa49b0c48c3edb83139543d"
 					client.authLine = authLine
 					return DigestAuth(authLine, method, client.URL)
 				} else if strings.IndexAny(authLine, "Basic") == 0 {
-					// not support yet
-					// TODO..
+					client.authLine = authLine
+					return BasicAuth(authLine, method, client.URL)
 				}
 
 			}
@@ -164,9 +195,8 @@ func (client *RTSPClient) checkAuth(method string, resp *Response) (string, erro
 				client.authLine = authLine
 				return DigestAuth(authLine, method, client.URL)
 			} else if strings.IndexAny(authLine, "Basic") == 0 {
-				// not support yet
-				// TODO..
-				return "", fmt.Errorf("not support Basic auth yet")
+				client.authLine = authLine
+				return BasicAuth(authLine, method, client.URL)
 			}
 		}
 	}
